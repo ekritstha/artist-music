78504944799064e6046b6e4540d1fb549cd63a82
ekritstha <akritshrestha@gmail.com>
2023-05-23 13:44:39 +0545

    user factory modified

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,15 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name' => $this->faker->word(),
+            'last_name' => $this->faker->word(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'phone' => '1',
+            'dob' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
+            'gender' => $this->faker->randomElement(['m','f','o']),
+            'address' => $this->faker->address(),
             'remember_token' => Str::random(10),
         ];
     }
